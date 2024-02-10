63b74e955e97a3ed9e96190e14ab27d6b9ef15ca
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2024-02-10 21:36:03 +0600

    added inventory

---
+++ src/lib/Components/ProductCard.svelte
@@ -0,0 +1,98 @@
+<script lang="ts">
+  import type { SizeType } from "$lib/types";
+  import { twMerge } from "tailwind-merge";
+  import type { ComponentProps } from "svelte";
+  import { Frame } from "flowbite-svelte";
+
+  export let href: string | null | undefined = undefined;
+  export let horizontal: boolean = false;
+  export let reverse: boolean = false;
+  export let img: string | undefined = undefined;
+  export let padding: SizeType | "none" = "lg";
+  export let size: SizeType | "none" = "sm";
+
+  // propagate props type from underlying Frame
+  interface $$Props extends ComponentProps<Frame> {
+    horizontal?: boolean;
+    reverse?: boolean;
+    img?: string;
+    padding?: SizeType | "none";
+    size?: SizeType | "none";
+    imgClass?: string;
+  }
+
+  const paddings: Record<SizeType | "none", string> = {
+    none: "",
+    xs: "p-2",
+    sm: "p-4",
+    md: "p-4 sm:p-5",
+    lg: "p-4 sm:p-6",
+    xl: "p-4 sm:p-8",
+  };
+
+  const sizes: Record<SizeType | "none", string> = {
+    none: "",
+    xs: "max-w-xs",
+    sm: "max-w-sm",
+    md: "max-w-xl",
+    lg: "max-w-2xl",
+    xl: "max-w-screen-xl",
+  };
+
+  let innerPadding: string;
+  $: innerPadding = paddings[padding];
+
+  let cardClass: string;
+  $: cardClass = twMerge(
+    "flex w-full",
+    sizes[size],
+    reverse ? "flex-col-reverse" : "flex-col",
+    horizontal && (reverse ? "md:flex-row-reverse" : "md:flex-row"),
+    href && "hover:bg-gray-100 dark:hover:bg-gray-700",
+    !img && innerPadding,
+    $$props.class
+  );
+
+  export let imgClass: string;
+  $: imgClass = twMerge(
+    reverse ? "rounded-b-lg" : "rounded-t-lg h-96",
+    horizontal && "object-cover w-full h-96 md:h-auto md:w-48 md:rounded-none",
+    horizontal && (reverse ? "md:rounded-e-lg" : "md:rounded-s-lg")
+  );
+</script>
+
+<Frame
+  tag={href ? "a" : "div"}
+  rounded
+  shadow
+  border
+  on:click
+  on:focusin
+  on:focusout
+  on:mouseenter
+  on:mouseleave
+  {href}
+  {...$$restProps}
+  class={cardClass}
+>
+  {#if img}
+    <img class={imgClass} src={img} alt="" />
+    <div class={innerPadding}>
+      <slot />
+    </div>
+  {:else}
+    <slot />
+  {/if}
+</Frame>
+
+<!--
+  @component
+  [Go to docs](https://flowbite-svelte.com/)
+  ## Props
+  @prop export let href: string | null | undefined = undefined;
+  @prop export let horizontal: boolean = false;
+  @prop export let reverse: boolean = false;
+  @prop export let img: string | undefined = undefined;
+  @prop export let padding: SizeType | 'none' = 'lg';
+  @prop export let size: SizeType | 'none' = 'sm';
+  -->
--- src/routes/(protected)/agent/inventory/+page.svelte
+++ src/routes/(protected)/agent/inventory/+page.svelte
@@ -4,24 +4,36 @@
   import Header from "../../Header.svelte";
   import ProductCard from "../../ProductCard.svelte";
 
+  import { page } from "$app/stores";
+
   let focused = false;
 
-  let inventory = [ {name: "KUE", quantity: 3, price: 23}, {name: "Khaan", quantity: 3, price: 23}, {name: "Zaura", quantity: 3, price: 23} ]
+  // let inventory = [ {name: "KUE", quantity: 3, price: 23}, {name: "Khaan", quantity: 3, price: 23}, {name: "Zaura", quantity: 3, price: 23} ]
+  let data = $page.data;
+  // let inventory = $page.data;
+  console.log(data);
+  let inventory = data.inventory;
 </script>
 
 <div class="flex flex-row">
-    {#if focused}
-      <Sidebar bind:focused />
-    {:else}
-      <RolledSidebar bind:focused />
-    {/if}
-    <div class="w-full h-screen p-5">
-        <Header page="Inventory" />
+  {#if focused}
+    <Sidebar bind:focused />
+  {:else}
+    <RolledSidebar bind:focused />
+  {/if}
+  <div class="w-full h-screen p-5">
+    <Header page="Inventory" />
 
-        <div class="grid grid-cols-4 gap-7">
-            {#each inventory as { name, quantity, price }, i}
-                <ProductCard name={name} quantity={quantity} unitPrice={price} />
-	        {/each}
-        </div>
+    <div class="grid grid-cols-5 gap-7">
+      {#each inventory as { name, amount, unitPrice, unit, imageLink }, i}
+        <ProductCard
+          {name}
+          quantity={amount}
+          {unitPrice}
+          {unit}
+          img={imageLink}
+        />
+      {/each}
     </div>
+  </div>
 </div>
