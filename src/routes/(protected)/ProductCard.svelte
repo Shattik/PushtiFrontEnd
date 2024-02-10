2e6c0a12b4073518a8713933267eb274f7cda597
Shattik <1905008@ugrad.cse.buet.ac.bd>
2024-02-10 18:15:32 +0600

    inventoryStarted

---
+++ src/routes/(protected)/ProductCard.svelte
@@ -0,0 +1,34 @@
+<script>
+    import { Card, Kbd, Table, TableBody, TableBodyCell, TableBodyRow, Button } from 'flowbite-svelte';
+
+    export let name = "Beaf Meat";
+    export let quantity = 10;
+    export let unitPrice = 100;
+    export let unit = "kg";
+    export let img = "https://media.post.rvohealth.io/wp-content/uploads/2020/08/beef-update-732x549-thumbnail.jpg";
+  </script>
+  
+  <div  class="space-y-4 w-full">
+    <Card {img} class="max-w-full">
+      <h5 class="mb-2 text-3xl font-bold tracking-tight text-[#308140] dark:text-white text-center">{name}</h5>
+
+      <hr class="mt-3 border-divider_col shadow"/>
+    <hr class="mb-3 border-divider_col shadow"/>
+
+    <Table divClass="overflow-clip">
+    <TableBody>
+        <TableBodyRow>
+          <TableBodyCell class="text-xl text-[#45B577]">Amount:</TableBodyCell>
+          <TableBodyCell><Button class="w-20 text-lg" color="green">{quantity}</Button></TableBodyCell>
+          <TableBodyCell class="text-xl text-[#45B577]">{unit}</TableBodyCell>
+        </TableBodyRow>
+        <TableBodyRow>
+          <TableBodyCell class="text-xl text-[#45B577]">Unit Price:</TableBodyCell>
+          <TableBodyCell><Button class="w-20 text-lg" color="green">{unitPrice}</Button></TableBodyCell>
+          <TableBodyCell class="text-xl text-[#45B577]">Tk</TableBodyCell>
+        </TableBodyRow>
+    </TableBody>
+    </Table>
+
+    </Card>
+  </div>
---
+++ src/routes/(protected)/agent/inventory/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+  import Sidebar from "../Sidebar.svelte";
+  import RolledSidebar from "../RolledSidebar.svelte";
+  import Header from "../../Header.svelte";
+  import ProductCard from "../../ProductCard.svelte";
+
+  let focused = false;
+
+  let inventory = [ {name: "KUE", quantity: 3, price: 23}, {name: "Khaan", quantity: 3, price: 23}, {name: "Zaura", quantity: 3, price: 23} ]
+</script>
+
+<div class="flex flex-row">
+    {#if focused}
+      <Sidebar bind:focused />
+    {:else}
+      <RolledSidebar bind:focused />
+    {/if}
+    <div class="w-full h-screen p-5">
+        <Header page="Inventory" />
+
+        <div class="grid grid-cols-4 gap-7">
+            {#each inventory as { name, quantity, price }, i}
+                <ProductCard name={name} quantity={quantity} unitPrice={price} />
+	        {/each}
+        </div>
+    </div>
+</div>
