66674f0806a16ae2f0992e039f23fe4c6738a457
Shattik <1905008@ugrad.cse.buet.ac.bd>
2024-02-11 01:32:49 +0600

    inventoryFiltered

--- src/routes/(protected)/agent/inventory/+page.svelte
+++ src/routes/(protected)/agent/inventory/+page.svelte
@@ -3,16 +3,23 @@
   import RolledSidebar from "../RolledSidebar.svelte";
   import Header from "../../Header.svelte";
   import ProductCard from "../../ProductCard.svelte";
+  import { Search } from 'flowbite-svelte';
 
   import { page } from "$app/stores";
 
   let focused = false;
+  let value = "";
+
 
   // let inventory = [ {name: "KUE", quantity: 3, price: 23}, {name: "Khaan", quantity: 3, price: 23}, {name: "Zaura", quantity: 3, price: 23} ]
   let data = $page.data;
   // let inventory = $page.data;
   console.log(data);
-  let inventory = data.inventory;
+  let mainInventory = data.inventory;
+  let inventory;
+
+  
+  $:inventory = mainInventory.filter((item) => item.name.toLowerCase().includes(value.toLowerCase())); 
 </script>
 
 <div class="flex flex-row">
@@ -23,8 +30,8 @@
   {/if}
   <div class="w-full h-screen p-5">
     <Header page="Inventory" />
-
-    <div class="grid grid-cols-5 gap-7">
+    <Search placeholder="Search Products" bind:value></Search>
+    <div class="grid grid-cols-5 md:grid-cols-4 sm:grid-cols-2 gap-7 mt-10 mb-5">
       {#each inventory as { name, amount, unitPrice, unit, imageLink }, i}
         <ProductCard
           {name}
