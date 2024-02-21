852291d8c15b4dced5c0735063927b73bdacfe8e
tahmid-404-20 <1905002@ugrad.cse.buet.ac.bd>
2024-02-21 23:09:12 +0600

    tried leaderboard, but gg

---
+++ src/routes/(protected)/agent/leaderboard/+page.js
@@ -0,0 +1,21 @@
+import { get } from "svelte/store";
+import { jwtToken } from "$lib/Components/token.js";
+import { PUBLIC_API_GATEWAY_URL } from "$env/static/public";
+
+export const ssr = false;
+
+export async function load() {
+  console.log("load");
+  console.log(get(jwtToken));
+  console.log("Bhai req ditasi");
+  const response = await fetch(`${PUBLIC_API_GATEWAY_URL}/agent/leaderboard`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      authorization: get(jwtToken),
+    },
+  });
+  const data = await response.json();
+  console.log(data);
+  return data;
+}
