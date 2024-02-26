5bac8fcf778331e915b7a12c574b837d164eb0ef
Shattik <1905008@ugrad.cse.buet.ac.bd>
2024-02-26 19:54:05 +0600

    loadingAdded

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,2 +1,11 @@
-<script>import "../app.pcss";</script><slot></slot>
+<script>
+    import "../app.pcss";
+    import Loading from "./loading.svelte";
+    import { navigating } from '$app/stores';
+</script>
+{#if $navigating}
+	<Loading />
+{:else}
+	<slot />
+{/if}
 
---
+++ src/routes/loading.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+    import { Stretch, SyncLoader, Wave, Pulse, Shadow, Moon, Plane } from 'svelte-loading-spinners';
+
+    let val = 0;
+
+    onMount(() => {
+        val = Math.floor((Math.random() * 100))%7;
+    });
+</script>
+
+<center class="grid grid-cols-1 place-items-center h-screen w-screen">
+{#if val == 0}
+    <Stretch size="100" color="#27c848"/>
+{:else if val == 1}
+    <SyncLoader size="100" color="#27c848"/>
+{:else if val == 2}
+    <Wave size="100" color="#27c848"/>
+{:else if val == 3}
+    <Pulse size="100" color="#27c848"/>
+{:else if val == 4}
+    <Shadow size="100" color="#27c848"/>
+{:else if val == 5}
+    <Moon size="100" color="#27c848"/>
+{:else if val == 6}
+    <Plane size="100" color="#27c848"/>
+{/if}
+</center>
+
